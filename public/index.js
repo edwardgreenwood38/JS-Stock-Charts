7c416f77fdd0960542c4e349af54d856a2d38cf6
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-12-10 18:55:41 -0500

    API copy from site

--- public/index.js
+++ public/index.js
@@ -6,6 +6,11 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+//     const response = fetch('https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1day&apikey=a0e2fca97e044c11acb86f89c5cf6b3c');
+//     let jsonData = response.json();
+//     console.log(jsonData);
+
+
 }
 
 main()
