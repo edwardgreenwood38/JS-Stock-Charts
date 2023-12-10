ca189e15d024118792604adca451fe93e616f86f
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-12-10 18:44:14 -0500

    add key

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 
 
 async function main() {
-
+    //apikey  a0e2fca97e044c11acb86f89c5cf6b3c
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
