689847a85d91fccdeb757b60e7eb420be21d3b0e
Edward E Greenwood <edwardgreenwood38@gmail.com>
2023-12-10 19:02:50 -0500

    step 2 completed with mockdata

--- public/index.js
+++ public/index.js
@@ -6,11 +6,28 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
-//     const response = fetch('https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1day&apikey=a0e2fca97e044c11acb86f89c5cf6b3c');
-//     let jsonData = response.json();
-//     console.log(jsonData);
+    // const response = fetch('https://api.twelvedata.com/time_series?symbol=BNTX,DIS,GME,MSFTX&interval=1day&apikey=a0e2fca97e044c11acb86f89c5cf6b3c');
+    // let jsonData = response.json();
+    // console.log(jsonData);
+    //****************************** note: not getting expect joson formatted data back ******************************/
 
+    
+    //========  example code
+    let result = mockData;
+    let GME = result.GME
+    let MSFT = result.MSFT
+    let DIS = result.DIS
+    let BNTX = result.BNTX
 
+    const stocks = [GME, MSFT, DIS, BNTX];
+
+    // Bonus Note: 
+    // Another way to write the above lines would to refactor it as:
+    // const {GME, MSFT, DIS, BTNX} = result 
+    // This is an example of "destructuring" an object
+    // "Destructuring" creates new variables from an object or an array
+
+    console.log(GME)
 }
 
 main()
